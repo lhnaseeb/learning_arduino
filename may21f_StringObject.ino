157e32ccb811bde8969c29f7522c3f5f118d4d06
lhnaseeb <46655419+lhnaseeb@users.noreply.github.com>
2019-05-21 06:34:35 +0300

    Add files via upload

--- may21f_StringObject.ino
+++ may21f_StringObject.ino
@@ -1,17 +1,25 @@
+String input = "Eating trees is great!";
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 
-  String myString = "hello, world";
+  input.toLowerCase();
+  input.replace("e","3");
+  input.replace("a","4");
+  input.replace("i","1");
 
-  myString.toUpperCase();
-
-  myString.concat("lings");
-
-  Serial.println(myString);
+  Serial.println(input);
+ 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
 }
+/* String myString = "hello, world";
+
+  myString.toUpperCase();
+
+  myString.concat("lings");
+
+  Serial.println(myString);*/

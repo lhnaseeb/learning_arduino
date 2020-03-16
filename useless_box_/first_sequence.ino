dad692d3f8a6fcda5a069ff41ee447a129c0eb52
lhnaseeb <46655419+lhnaseeb@users.noreply.github.com>
2020-03-16 00:43:11 -0400

    Add files via upload

---
+++ useless_box_/first_sequence.ino
@@ -0,0 +1,23 @@
+/*
+Adafruit Arduino - Lesson 4. 8 LEDs and a Shift Register
+*/
+ 
+void firstShifty() 
+{
+  leds = 0;
+  updateShiftRegister();
+  delay(500);
+  for (int i = 0; i < 8; i++)
+  {
+    bitSet(leds, i);
+    updateShiftRegister();
+    delay(500);
+  }
+}
+ 
+void updateShiftRegister()
+{
+   digitalWrite(latchPin, LOW);
+   shiftOut(dataPin, clockPin, LSBFIRST, leds);
+   digitalWrite(latchPin, HIGH);
+}
